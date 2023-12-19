71a47fc5daf80902a2c933402466a8b238241127
pedro-dim <pcontato19@gmail.com>
2023-12-19 22:18:01 +0000

    Add Active Tab toggle panel

---
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Pages;
+
+use Filament\Actions;
+use Filament\Resources\Components\Tab;
+use App\Filament\Resources\UserResource;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+
+class ListUsers extends ListRecords
+{
+    protected static string $resource = UserResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'active' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('active', true)),
+            'inactive' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('active', false)),
+        ];
+    }
+}
